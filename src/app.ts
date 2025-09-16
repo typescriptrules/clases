62ae6d1e096d3762af284e414ce71e9e3edfd53c
Kale-8 <kaled.geme2@gmail.com>
2025-09-16 09:44:46 -0500

    few changes

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,26 @@ import { router, initRoutes } from "./routes/index.ts";
 const port = process.env.PORT || 3001;
 const app = express ();
 
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    // if (!origin) return callback(null, true);
+    if (origin === "http://localhost:5173") {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 await initRoutes();
-app.use("/", router);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}!`);
 })
 
+//create frontend
 //use cors for unique origin
-//change book to person
 //add any kin of middleware
