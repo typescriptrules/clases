7ad19bd8f63f1314d8683c8db1bde83540c1c201
Quiro66 <66quiro@gmail.com>
2025-09-16 08:30:50 -0500

    Relizacion de actividad crud book

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,15 +1,17 @@
 import { Router, type Request, type Response } from 'express'
-import { createBook, getBook, getBooks } from '../controllers/books.ts'
+import { createBook, getBook, getBooks, updateBook, deleteBooks } from '../controllers/books.ts'
 
 const router:Router = Router()
 /**
- * http://localhost:3002/books
- */
 
-router.get("/", (req:Request, res:Response)=> {
-    getBooks(req, res)
-})
+ http://localhost:3002/books*/
+
+router.get("/", getBooks);
 router.get('/:id', getBook)
 router.post('/', createBook)
+router.patch('/:id', updateBook)
+router.delete('/:id', deleteBooks)
+
+
 
 export { router }
