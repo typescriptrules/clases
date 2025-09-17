67a414ff05c64d7d29d44670943a666e6f6e3d3c
Susana Silva Vallejo <susilvav@unal.edu.co>
2025-09-17 07:57:14 -0500

    feat: add middlewares for validation

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,7 @@
 export interface IUser {
     id: string;
     name:string;
+    role: "admin" | "user";
     age: number;
     email: string;
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,31 +1,33 @@
 import { Router, type Request, type Response } from "express";
 import { getUser, getUsers, createUser, deleteUser, updateUser } from "../controllers/users.ts";
+import { validateUserData, validateUniqueUserId} from '../middlewares/validators.ts';
 
-const router:Router = Router();
+const router: Router = Router();
 
-router.get('/:id', (req:Request<{ id: string }>, res: Response) => {
-    console.log(`Printing user ${req.params.id}`);
-    getUser(req, res);
+router.get("/:id", (req: Request<{ id: string }>, res: Response) => {
+  console.log(`Printing user ${req.params.id}`);
+  getUser(req, res);
 });
 
-router.get('/', (req:Request, res:Response) => {
-    console.log('Printing users');
-    getUsers(req, res);
+router.get("/", (req: Request, res: Response) => {
+  console.log("Listing users");
+  getUsers(req, res);
 });
 
-router.post('/', (req: Request, res: Response) => {
-  console.log('Creating user');
+router.post("/", validateUserData, validateUniqueUserId, (req: Request, res: Response) => {
+  console.log("Creating user");
   createUser(req, res);
 });
 
-router.delete('/:id', (req: Request<{ id: string }>, res: Response) => {
+router.delete("/:id", (req: Request<{ id: string }>, res: Response) => {
   console.log(`Deleting user ${req.params.id}`);
   deleteUser(req, res);
 });
 
-router.put('/:id', (req: Request<{ id: string }>, res: Response) => {
-  console.log(`Updating user ${req.params.id}`);
-  updateUser(req, res);
-});
+router.put( "/:id", validateUserData, (req: Request<{ id: string }>, res: Response) => {
+    console.log(`Updating user ${req.params.id}`);
+    updateUser(req, res);
+  }
+);
 
 export { router };
